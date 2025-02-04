d47710ca1559de96d228138de6018c1665659f5e
Travis Huss <travisjhuss@gmail.com>
2025-02-04 13:27:46 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",
     reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "/",
+    },
 };
 
 export default nextConfig;
