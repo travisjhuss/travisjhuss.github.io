cd09317264db05584f9218cb30a9283c1cf1cfcf
Travis Huss <travisjhuss@gmail.com>
2025-02-04 12:53:49 -0600

    update build

---
+++ .next/server/chunks/387.js
@@ -0,0 +1,74 @@
+"use strict";exports.id=387,exports.ids=[387],exports.modules={6387:(e,t,n)=>{n.d(t,{A:()=>N});var r=n(6177),i=n.n(r),o=n(339),u=n(28);function l(e){try{return e.matches(":focus-visible")}catch(e){}return!1}var s=n(5369),a=n(8420),c=n(5212);let d=n(8264).A,p={};function h(e,t){let n=r.useRef(p);return n.current===p&&(n.current=e(t)),n}class f{static create(){return new f}static use(){let e=h(f.create).current,[t,n]=r.useState(!1);return e.shouldMount=t,e.setShouldMount=n,r.useEffect(e.mountEffect,[t]),e}constructor(){this.mountEffect=()=>{this.shouldMount&&!this.didMount&&null!==this.ref.current&&(this.didMount=!0,this.mounted.resolve())},this.ref={current:null},this.mounted=null,this.didMount=!1,this.shouldMount=!1,this.setShouldMount=null}mount(){return this.mounted||(this.mounted=function(){let e,t;let n=new Promise((n,r)=>{e=n,t=r});return n.resolve=e,n.reject=t,n}(),this.shouldMount=!0,this.setShouldMount(this.shouldMount)),this.mounted}start(...e){this.mount().then(()=>this.ref.current?.start(...e))}stop(...e){this.mount().then(()=>this.ref.current?.stop(...e))}pulsate(...e){this.mount().then(()=>this.ref.current?.pulsate(...e))}}var m=n(4990),b=n(8463),v=n(401),g=n(329);function y(e,t){var n=Object.create(null);return e&&r.Children.map(e,function(e){return e}).forEach(function(e){n[e.key]=t&&(0,r.isValidElement)(e)?t(e):e}),n}function x(e,t,n){return null!=n[t]?n[t]:e.props[t]}var M=Object.values||function(e){return Object.keys(e).map(function(t){return e[t]})},E=function(e){function t(t,n){var r,i=(r=e.call(this,t,n)||this).handleExited.bind(function(e){if(void 0===e)throw ReferenceError("this hasn't been initialised - super() hasn't been called");return e}(r));return r.state={contextValue:{isMounting:!0},handleExited:i,firstRender:!0},r}(0,v.A)(t,e);var n=t.prototype;return n.componentDidMount=function(){this.mounted=!0,this.setState({contextValue:{isMounting:!1}})},n.componentWillUnmount=function(){this.mounted=!1},t.getDerivedStateFromProps=function(e,t){var n,i,o=t.children,u=t.handleExited;return{children:t.firstRender?y(e.children,function(t){return(0,r.cloneElement)(t,{onExited:u.bind(null,t),in:!0,appear:x(t,"appear",e),enter:x(t,"enter",e),exit:x(t,"exit",e)})}):(Object.keys(i=function(e,t){function n(n){return n in t?t[n]:e[n]}e=e||{},t=t||{};var r,i=Object.create(null),o=[];for(var u in e)u in t?o.length&&(i[u]=o,o=[]):o.push(u);var l={};for(var s in t){if(i[s])for(r=0;r<i[s].length;r++){var a=i[s][r];l[i[s][r]]=n(a)}l[s]=n(s)}for(r=0;r<o.length;r++)l[o[r]]=n(o[r]);return l}(o,n=y(e.children))).forEach(function(t){var l=i[t];if((0,r.isValidElement)(l)){var s=t in o,a=t in n,c=o[t],d=(0,r.isValidElement)(c)&&!c.props.in;a&&(!s||d)?i[t]=(0,r.cloneElement)(l,{onExited:u.bind(null,l),in:!0,exit:x(l,"exit",e),enter:x(l,"enter",e)}):a||!s||d?a&&s&&(0,r.isValidElement)(c)&&(i[t]=(0,r.cloneElement)(l,{onExited:u.bind(null,l),in:c.props.in,exit:x(l,"exit",e),enter:x(l,"enter",e)})):i[t]=(0,r.cloneElement)(l,{in:!1})}}),i),firstRender:!1}},n.handleExited=function(e,t){var n=y(this.props.children);e.key in n||(e.props.onExited&&e.props.onExited(t),this.mounted&&this.setState(function(t){var n=(0,b.A)({},t.children);return delete n[e.key],{children:n}}))},n.render=function(){var e=this.props,t=e.component,n=e.childFactory,r=(0,m.A)(e,["component","childFactory"]),o=this.state.contextValue,u=M(this.state.children).map(n);return(delete r.appear,delete r.enter,delete r.exit,null===t)?i().createElement(g.A.Provider,{value:o},u):i().createElement(g.A.Provider,{value:o},i().createElement(t,r,u))},t}(i().Component);E.propTypes={},E.defaultProps={component:"div",childFactory:function(e){return e}};let A=[];class R{static create(){return new R}start(e,t){this.clear(),this.currentId=setTimeout(()=>{this.currentId=null,t()},e)}constructor(){this.currentId=null,this.clear=()=>{null!==this.currentId&&(clearTimeout(this.currentId),this.currentId=null)},this.disposeEffect=()=>this.clear}}var P=n(7902),k=n(7120),j=n(8594);let w=(0,j.A)("MuiTouchRipple",["root","ripple","rippleVisible","ripplePulsate","child","childLeaving","childPulsate"]),T=(0,P.i7)`
+  0% {
+    transform: scale(0);
+    opacity: 0.1;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 0.3;
+  }
+`,S=(0,P.i7)`
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+`,V=(0,P.i7)`
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(0.92);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`,$=(0,s.default)("span",{name:"MuiTouchRipple",slot:"Root"})({overflow:"hidden",pointerEvents:"none",position:"absolute",zIndex:0,top:0,right:0,bottom:0,left:0,borderRadius:"inherit"}),C=(0,s.default)(function(e){let{className:t,classes:n,pulsate:i=!1,rippleX:u,rippleY:l,rippleSize:s,in:a,onExited:c,timeout:d}=e,[p,h]=r.useState(!1),f=(0,o.A)(t,n.ripple,n.rippleVisible,i&&n.ripplePulsate),m=(0,o.A)(n.child,p&&n.childLeaving,i&&n.childPulsate);return a||p||h(!0),r.useEffect(()=>{if(!a&&null!=c){let e=setTimeout(c,d);return()=>{clearTimeout(e)}}},[c,a,d]),(0,k.jsx)("span",{className:f,style:{width:s,height:s,top:-(s/2)+l,left:-(s/2)+u},children:(0,k.jsx)("span",{className:m})})},{name:"MuiTouchRipple",slot:"Ripple"})`
+  opacity: 0;
+  position: absolute;
+
+  &.${w.rippleVisible} {
+    opacity: 0.3;
+    transform: scale(1);
+    animation-name: ${T};
+    animation-duration: ${550}ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+  }
+
+  &.${w.ripplePulsate} {
+    animation-duration: ${({theme:e})=>e.transitions.duration.shorter}ms;
+  }
+
+  & .${w.child} {
+    opacity: 1;
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: currentColor;
+  }
+
+  & .${w.childLeaving} {
+    opacity: 0;
+    animation-name: ${S};
+    animation-duration: ${550}ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+  }
+
+  & .${w.childPulsate} {
+    position: absolute;
+    /* @noflip */
+    left: 0px;
+    top: 0;
+    animation-name: ${V};
+    animation-duration: 2500ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+    animation-iteration-count: infinite;
+    animation-delay: 200ms;
+  }
+`,I=r.forwardRef(function(e,t){let{center:n=!1,classes:i={},className:u,...l}=(0,a.b)({props:e,name:"MuiTouchRipple"}),[s,c]=r.useState([]),d=r.useRef(0),p=r.useRef(null);r.useEffect(()=>{p.current&&(p.current(),p.current=null)},[s]);let f=r.useRef(!1),m=function(){var e;let t=h(R.create).current;return e=t.disposeEffect,r.useEffect(e,A),t}(),b=r.useRef(null),v=r.useRef(null),g=r.useCallback(e=>{let{pulsate:t,rippleX:n,rippleY:r,rippleSize:u,cb:l}=e;c(e=>[...e,(0,k.jsx)(C,{classes:{ripple:(0,o.A)(i.ripple,w.ripple),rippleVisible:(0,o.A)(i.rippleVisible,w.rippleVisible),ripplePulsate:(0,o.A)(i.ripplePulsate,w.ripplePulsate),child:(0,o.A)(i.child,w.child),childLeaving:(0,o.A)(i.childLeaving,w.childLeaving),childPulsate:(0,o.A)(i.childPulsate,w.childPulsate)},timeout:550,pulsate:t,rippleX:n,rippleY:r,rippleSize:u},d.current)]),d.current+=1,p.current=l},[i]),y=r.useCallback((e={},t={},r=()=>{})=>{let i,o,u;let{pulsate:l=!1,center:s=n||t.pulsate,fakeElement:a=!1}=t;if(e?.type==="mousedown"&&f.current){f.current=!1;return}e?.type==="touchstart"&&(f.current=!0);let c=a?null:v.current,d=c?c.getBoundingClientRect():{width:0,height:0,left:0,top:0};if(!s&&void 0!==e&&(0!==e.clientX||0!==e.clientY)&&(e.clientX||e.touches)){let{clientX:t,clientY:n}=e.touches&&e.touches.length>0?e.touches[0]:e;i=Math.round(t-d.left),o=Math.round(n-d.top)}else i=Math.round(d.width/2),o=Math.round(d.height/2);s?(u=Math.sqrt((2*d.width**2+d.height**2)/3))%2==0&&(u+=1):u=Math.sqrt((2*Math.max(Math.abs((c?c.clientWidth:0)-i),i)+2)**2+(2*Math.max(Math.abs((c?c.clientHeight:0)-o),o)+2)**2),e?.touches?null===b.current&&(b.current=()=>{g({pulsate:l,rippleX:i,rippleY:o,rippleSize:u,cb:r})},m.start(80,()=>{b.current&&(b.current(),b.current=null)})):g({pulsate:l,rippleX:i,rippleY:o,rippleSize:u,cb:r})},[n,g,m]),x=r.useCallback(()=>{y({},{pulsate:!0})},[y]),M=r.useCallback((e,t)=>{if(m.clear(),e?.type==="touchend"&&b.current){b.current(),b.current=null,m.start(0,()=>{M(e,t)});return}b.current=null,c(e=>e.length>0?e.slice(1):e),p.current=t},[m]);return r.useImperativeHandle(t,()=>({pulsate:x,start:y,stop:M}),[x,y,M]),(0,k.jsx)($,{className:(0,o.A)(w.root,i.root,u),ref:v,...l,children:(0,k.jsx)(E,{component:null,exit:!0,children:s})})});var O=n(2506);function B(e){return(0,O.Ay)("MuiButtonBase",e)}let D=(0,j.A)("MuiButtonBase",["root","disabled","focusVisible"]),L=e=>{let{disabled:t,focusVisible:n,focusVisibleClassName:r,classes:i}=e,o=(0,u.A)({root:["root",t&&"disabled",n&&"focusVisible"]},B,i);return n&&r&&(o.root+=` ${r}`),o},z=(0,s.default)("button",{name:"MuiButtonBase",slot:"Root",overridesResolver:(e,t)=>t.root})({display:"inline-flex",alignItems:"center",justifyContent:"center",position:"relative",boxSizing:"border-box",WebkitTapHighlightColor:"transparent",backgroundColor:"transparent",outline:0,border:0,margin:0,borderRadius:0,padding:0,cursor:"pointer",userSelect:"none",verticalAlign:"middle",MozAppearance:"none",WebkitAppearance:"none",textDecoration:"none",color:"inherit","&::-moz-focus-inner":{borderStyle:"none"},[`&.${D.disabled}`]:{pointerEvents:"none",cursor:"default"},"@media print":{colorAdjust:"exact"}});function F(e,t,n,r=!1){return d(i=>(n&&n(i),r||e[t](i),!0))}let N=r.forwardRef(function(e,t){let n=(0,a.b)({props:e,name:"MuiButtonBase"}),{action:i,centerRipple:u=!1,children:s,className:p,component:h="button",disabled:m=!1,disableRipple:b=!1,disableTouchRipple:v=!1,focusRipple:g=!1,focusVisibleClassName:y,LinkComponent:x="a",onBlur:M,onClick:E,onContextMenu:A,onDragLeave:R,onFocus:P,onFocusVisible:j,onKeyDown:w,onKeyUp:T,onMouseDown:S,onMouseLeave:V,onMouseUp:$,onTouchEnd:C,onTouchMove:O,onTouchStart:B,tabIndex:D=0,TouchRippleProps:N,touchRippleRef:H,type:W,..._}=n,U=r.useRef(null),X=f.use(),q=(0,c.A)(X.ref,H),[K,Y]=r.useState(!1);m&&K&&Y(!1),r.useImperativeHandle(i,()=>({focusVisible:()=>{Y(!0),U.current.focus()}}),[]);let G=X.shouldMount&&!b&&!m;r.useEffect(()=>{K&&g&&!b&&X.pulsate()},[b,g,K,X]);let J=F(X,"start",S,v),Q=F(X,"stop",A,v),Z=F(X,"stop",R,v),ee=F(X,"stop",$,v),et=F(X,"stop",e=>{K&&e.preventDefault(),V&&V(e)},v),en=F(X,"start",B,v),er=F(X,"stop",C,v),ei=F(X,"stop",O,v),eo=F(X,"stop",e=>{l(e.target)||Y(!1),M&&M(e)},!1),eu=d(e=>{U.current||(U.current=e.currentTarget),l(e.target)&&(Y(!0),j&&j(e)),P&&P(e)}),el=()=>{let e=U.current;return h&&"button"!==h&&!("A"===e.tagName&&e.href)},es=d(e=>{g&&!e.repeat&&K&&" "===e.key&&X.stop(e,()=>{X.start(e)}),e.target===e.currentTarget&&el()&&" "===e.key&&e.preventDefault(),w&&w(e),e.target===e.currentTarget&&el()&&"Enter"===e.key&&!m&&(e.preventDefault(),E&&E(e))}),ea=d(e=>{g&&" "===e.key&&K&&!e.defaultPrevented&&X.stop(e,()=>{X.pulsate(e)}),T&&T(e),E&&e.target===e.currentTarget&&el()&&" "===e.key&&!e.defaultPrevented&&E(e)}),ec=h;"button"===ec&&(_.href||_.to)&&(ec=x);let ed={};"button"===ec?(ed.type=void 0===W?"button":W,ed.disabled=m):(_.href||_.to||(ed.role="button"),m&&(ed["aria-disabled"]=m));let ep=(0,c.A)(t,U),eh={...n,centerRipple:u,component:h,disabled:m,disableRipple:b,disableTouchRipple:v,focusRipple:g,tabIndex:D,focusVisible:K},ef=L(eh);return(0,k.jsxs)(z,{as:ec,className:(0,o.A)(ef.root,p),ownerState:eh,onBlur:eo,onClick:E,onContextMenu:Q,onFocus:eu,onKeyDown:es,onKeyUp:ea,onMouseDown:J,onMouseLeave:et,onMouseUp:ee,onDragLeave:Z,onTouchEnd:er,onTouchMove:ei,onTouchStart:en,ref:ep,tabIndex:m?-1:D,type:W,...ed,..._,children:[s,G?(0,k.jsx)(I,{ref:q,center:u,...N}):null]})})},8264:(e,t,n)=>{n.d(t,{A:()=>o});var r=n(6177),i=n(1358);let o=function(e){let t=r.useRef(e);return(0,i.A)(()=>{t.current=e}),r.useRef((...e)=>(0,t.current)(...e)).current}},329:(e,t,n)=>{n.d(t,{A:()=>i});var r=n(6177);let i=n.n(r)().createContext(null)},401:(e,t,n)=>{function r(e,t){return(r=Object.setPrototypeOf?Object.setPrototypeOf.bind():function(e,t){return e.__proto__=t,e})(e,t)}function i(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,r(e,t)}n.d(t,{A:()=>i})},4990:(e,t,n)=>{n.d(t,{A:()=>r});function r(e,t){if(null==e)return{};var n={};for(var r in e)if(({}).hasOwnProperty.call(e,r)){if(t.includes(r))continue;n[r]=e[r]}return n}}};
